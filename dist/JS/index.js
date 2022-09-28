6a88c705d71bf6d611d116ccbac4cd7e65fd70a6
Brooklyn Kairupan <brokinignacio3@gmail.com>
2022-09-28 16:00:19 +0800

    second commit

---
+++ dist/JS/index.js
@@ -0,0 +1,5 @@
+const Hello =  document.querySelector('#hello')
+const HelloActive = document.querySelector('#helloActive')
+Hello.addEventListener ('click', function(){
+    HelloActive.classList.toggle('hello__active');
+})
